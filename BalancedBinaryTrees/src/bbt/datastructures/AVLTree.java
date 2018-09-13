ec5d44302c4666d081129168bdf3fdd9feafbcbd
Juha Harviainen <juha.harviainen@gmail.com>
2018-09-13 14:31:59 +0300

    Add AVLTree class

---
+++ BalancedBinaryTrees/src/bbt/datastructures/AVLTree.java
@@ -0,0 +1,16 @@
+package bbt.datastructures;
+
+public class AVLTree<T> extends BinaryTree {
+    private int height;
+    
+    @Override
+    public void insert(Comparable element) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void erase(Comparable element) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
