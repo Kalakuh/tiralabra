56d68644d303048fc2e66aca35a4d1da82b31fea
Juha Harviainen <juha.harviainen@gmail.com>
2018-09-13 19:15:56 +0300

    Start implementing AVL rotations

--- BalancedBinaryTrees/src/bbt/datastructure/AVLTree.java
+++ BalancedBinaryTrees/src/bbt/datastructure/AVLTree.java
@@ -56,7 +56,58 @@ public class AVLTree<T> extends BinaryTree {
         throw new UnsupportedOperationException("Not supported yet.");
     }
     
+    /**
+     * Returns the height of the binary tree
+     * @return The height of the binary tree
+     */
     private int getHeight () {
         return this.height;
+    }
+    
+    /**
+     * Updates the height variable of the binary tree
+     */
+    private void updateAndFixHeight () {
+        if (this.getValueAtRoot() == null) {
+            this.height = 0;
+            return;
+        }
+        this.height = 1;
+        if (this.getLeftChild() != null) {
+            this.height = Math.max(this.height, ((AVLTree<T>)this.getLeftChild()).getHeight());
+        }
+        if (this.getRightChild() != null) {
+            this.height = Math.max(this.height, ((AVLTree<T>)this.getRightChild()).getHeight());
+        }
+    }
+    
+    /**
+     * Rotates the tree clockwise as a part of maintaining the AVL condition
+     */
+    private void rotateClockwise () {
+        AVLTree left = (AVLTree)this.getLeftChild();
+        Comparable rootValue = this.getValueAtRoot();
+        Comparable leftValue = left.getValueAtRoot();
+        this.setValueAtRoot(leftValue);
+        this.setLeftChild(left.getLeftChild());
+        left.setLeftChild(left.getRightChild());
+        left.setRightChild((AVLTree)this.getRightChild());
+        left.setValueAtRoot(rootValue);
+        this.setRightChild(left);
+    }
+    
+    /**
+     * Rotates the tree counterclockwise as a part of maintaining the AVL condition
+     */
+    private void rotateCounterclockwise () {
+        AVLTree right = (AVLTree)this.getRightChild();
+        Comparable rootValue = this.getValueAtRoot();
+        Comparable rightValue = right.getValueAtRoot();
+        this.setValueAtRoot(rightValue);
+        this.setRightChild(right.getRightChild());
+        right.setRightChild(right.getLeftChild());
+        right.setLeftChild((AVLTree)this.getLeftChild());
+        right.setValueAtRoot(rootValue);
+        this.setLeftChild(right);
     }
 }
