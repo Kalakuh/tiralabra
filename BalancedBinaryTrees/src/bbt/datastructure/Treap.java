c694ee564e9c2cb1d37530ffbdbc2c52c0a3740e
Juha Harviainen <juha.harviainen@gmail.com>
2018-09-20 16:15:43 +0300

    Add priority function for Treap

--- BalancedBinaryTrees/src/bbt/datastructure/Treap.java
+++ BalancedBinaryTrees/src/bbt/datastructure/Treap.java
@@ -5,6 +5,9 @@ import java.util.Random;
 public class Treap<T> extends BinaryTree {
     private int priority;
     
+    /**
+     * Default constructor for treap
+     */
     public Treap () {
         Random random = new Random();
         this.priority = random.nextInt();
@@ -20,4 +23,11 @@ public class Treap<T> extends BinaryTree {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
+    /**
+     * Returns the priority of the treap node
+     * @return The priority of the treap
+     */
+    private int getPriority () {
+        return this.priority;
+    }
 }
