08807d4cd8caec09b9dfc38cb6d1d4d88378b7e6
Juha Harviainen <juha.harviainen@gmail.com>
2018-09-20 16:49:40 +0300

    Finish implementing Treap insert

--- BalancedBinaryTrees/src/bbt/datastructure/Treap.java
+++ BalancedBinaryTrees/src/bbt/datastructure/Treap.java
@@ -61,6 +61,37 @@ public class Treap<T> extends BinaryTree {
      * Check heap condition - that is, whether highest priorities are topmost - and possibly fix it
      */
     private void checkHeapCondition() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        // After insertion or deletion at most one of the children can have a priority higher than the priority of this node
+        if (this.getLeftChild() != null) {
+            int leftPriority = ((Treap<T>)this.getLeftChild()).getPriority();
+            if (leftPriority > this.priority) {
+                this.rotateClockwise();
+            }
+        }
+        if (this.getRightChild() != null) {
+            int rightPriority = ((Treap<T>)this.getRightChild()).getPriority();
+        }
+    }
+    
+    /**
+     * Rotates the tree clockwise and updates priorities
+     */
+    @Override
+    protected void rotateClockwise () {
+        int leftPriority = ((Treap<T>)this.getLeftChild()).getPriority();
+        ((Treap<T>)this.getLeftChild()).setPriority(this.priority);
+        this.setPriority(leftPriority);
+        super.rotateClockwise();
+    }
+    
+    /**
+     * Rotates the tree counterclockwise and updates priorities
+     */
+    @Override
+    protected void rotateCounterclockwise () {
+        int rightPriority = ((Treap<T>)this.getRightChild()).getPriority();
+        ((Treap<T>)this.getRightChild()).setPriority(this.priority);
+        this.setPriority(rightPriority);
+        super.rotateCounterclockwise();
     }
 }
