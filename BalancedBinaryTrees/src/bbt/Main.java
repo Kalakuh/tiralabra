f4462e2ba85cc5045d1a29b99f0da0b3e547ec31
Juha Harviainen <juha.harviainen@gmail.com>
2018-09-27 00:31:35 +0300

    Change the test in Main class

--- BalancedBinaryTrees/src/bbt/Main.java
+++ BalancedBinaryTrees/src/bbt/Main.java
@@ -1,22 +1,20 @@
 package bbt;
 
-import bbt.datastructure.AVLTree;
-import bbt.datastructure.BinaryTree;
-import bbt.datastructure.Treap;
+import bbt.datastructure.*;
 
 public class Main {
     /**
      * @param args the command line arguments
      */
     public static void main (String[] args) {
-        BinaryTree<Integer> tree = new Treap<>();
+        ScapegoatTree<Integer> tree = new ScapegoatTree<>();
         System.out.println("Tree contains 2: " + tree.contains(2));
-        tree.insert(2);
+        tree.insert(3);
         System.out.println("Tree contains 2: " + tree.contains(2));
         tree.insert(0);
         tree.insert(1);
         tree.insert(2);
-        tree.insert(3);
+        tree.insert(4);
         System.out.println("Tree contains 0: " + tree.contains(0));
         System.out.println("Tree contains 1: " + tree.contains(1));
         System.out.println("Tree contains 2: " + tree.contains(2));
