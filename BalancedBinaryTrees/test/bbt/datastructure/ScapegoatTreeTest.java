e89e7794bc0140e3f2338e774191dd02df11f732
Juha Harviainen <juha.harviainen@gmail.com>
2018-09-27 00:35:03 +0300

    Add basic unit testing for ScapegoatTree

---
+++ BalancedBinaryTrees/test/bbt/datastructure/ScapegoatTreeTest.java
@@ -0,0 +1,88 @@
+package bbt.datastructure;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ScapegoatTreeTest {
+    private ScapegoatTree<Integer> instance;
+    
+    @Before
+    public void setUp() {
+        instance = new ScapegoatTree<>();
+    }
+    
+    /**
+     * Test that inserting null is not allowed
+     */
+    @Test
+    public void testInsertingNullIsNotAllowed() {
+        try {
+            instance.insert(null);
+            fail("Program allowed inserting null");
+        } catch (IllegalArgumentException e) {
+            
+        }
+    }
+    
+    /**
+     * Test that inserted element can be found
+     */
+    @Test
+    public void testInsertedElementIsFound() {
+        instance.insert(1);
+        instance.insert(3);
+        instance.insert(2);
+        assertTrue(instance.contains(2));
+    }
+
+    /**
+     * Test that erasing an inserted element from the tree works
+     */
+    @Test
+    public void testInsertAndErase() {
+        instance.insert(1);
+        instance.insert(2);
+        assertTrue(instance.contains(2));
+        instance.erase(1);
+        assertTrue(!instance.contains(1));
+        assertTrue(instance.contains(2));
+    }
+    
+    /**
+     * Test erasing on larger trees 
+     */
+    @Test
+    public void testInsertAndEraseLarger() {
+        instance.insert(0);
+        instance.insert(1);
+        instance.insert(2);
+        instance.insert(3);
+        instance.insert(4);
+        instance.insert(8);
+        instance.insert(7);
+        instance.insert(6);
+        instance.insert(5);
+        instance.erase(3);
+        assertTrue(!instance.contains(3));
+        assertTrue(instance.contains(0));
+        assertTrue(instance.contains(1));
+        assertTrue(instance.contains(2));
+        assertTrue(instance.contains(4));
+        assertTrue(instance.contains(5));
+        assertTrue(instance.contains(6));
+        assertTrue(instance.contains(7));
+        assertTrue(instance.contains(8));
+    }
+    
+    /**
+     * Tests clearing the tree - if this works for AVL then we can assume it works for everything, since it's implemented in BinaryTree
+     */
+    @Test
+    public void testClear () {
+        instance.insert(1);
+        instance.insert(2);
+        instance.clear();
+        assertTrue(!instance.contains(2));
+    }
+}
