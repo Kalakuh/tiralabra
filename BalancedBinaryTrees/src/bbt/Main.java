818c847b6d06966b51d853bbb641a82eedb687cf
Juha Harviainen <juha.harviainen@gmail.com>
2018-10-05 17:26:16 +0300

    Add argument support for Main class

--- BalancedBinaryTrees/src/bbt/Main.java
+++ BalancedBinaryTrees/src/bbt/Main.java
@@ -2,22 +2,118 @@ package bbt;
 
 import bbt.datastructure.*;
 import bbt.testing.*;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintStream;
 
 public class Main {
+    private static ByteArrayOutputStream baos;
+    private static boolean interactive = false;
+    private static boolean fileInput = false;
+    private static String inputPath = "";
+    private static boolean fileOutput = false;
+    private static String outputPath = "";
+    private static boolean gui = false;
+    
+    private static void init() {
+        interactive = false;
+        fileInput = false;
+        inputPath = "";
+        fileOutput = false;
+        outputPath = "";
+        gui = false;
+    }
+    
     /**
-     * The main function... Currently runs simple tests.
+     * The main function, handles argument logic.
+     * -f file      read input from file
+     * -o file      output data to file
+     * -i           enable interactive mode
+     * -g           enable graphical user interface
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Test test = new RandomNCommandsTest(1000000, 1);
+        init();
+        analyseArgs(args);
         
-        Tester avlTester = new Tester(new AVLTree());
-        avlTester.runTest(test);
+        if (fileOutput) {
+            baos = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(baos));
+        }
+        if (fileInput && interactive && gui) {
+            System.out.println("Flags '-f', '-i' and '-g' can not be used at the same time.");
+        } else if (fileInput && interactive) {
+            System.out.println("Flags '-f' and '-i' can not be used at the same time.");
+        } else if (fileInput && gui) {
+            System.out.println("Flags '-f' and '-g' can not be used at the same time.");
+        } else if (interactive && gui) {
+            System.out.println("Flags '-i' and '-g' can not be used at the same time.");
+        } else if (interactive) {
+            
+        } else if (fileInput) {
+            
+        } else if (gui) {
+            System.out.println("Graphical user interface has not been implemented yet.");
+        } else {
+            System.out.println("No special flags were found - running default tests.");
+            Test test = new RandomNCommandsTest(1000000, 1);
+
+            Tester avlTester = new Tester(new AVLTree());
+            avlTester.runTest(test);
+
+            Tester treapTester = new Tester(new Treap());
+            treapTester.runTest(test);
+
+            Tester scapegoatTester = new Tester(new ScapegoatTree());
+            scapegoatTester.runTest(test);
+        }
         
-        Tester treapTester = new Tester(new Treap());
-        treapTester.runTest(test);
-        
-        Tester scapegoatTester = new Tester(new ScapegoatTree());
-        scapegoatTester.runTest(test);
+        try {
+            if (fileOutput) {
+                FileWriter writer = new FileWriter(new File(outputPath));
+                writer.write(baos.toString());
+                writer.close();
+            }
+        } catch (IOException e) {
+            System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+            System.out.println("An error happened while trying to output the results into a file.");
+        }
+    }
+    
+    private static void analyseArgs(String[] args) {
+        String previousArg = "";
+        for (String arg : args) {
+            switch (arg) {
+                case "-i":
+                    interactive = true;
+                    break;
+                case "-o":
+                    fileOutput = true;
+                    break;
+                case "-f":
+                    fileInput = true;
+                    break;
+                case "-g":
+                    gui = true;
+                    break;
+                default:
+                    if (previousArg.equals("-o")) {
+                        outputPath = arg;
+                    } else if (previousArg.equals("-f")) {
+                        inputPath = arg;
+                    } else if (arg.startsWith("-")) {
+                        System.out.println("Unknown flag '" + arg + "'.");
+                        return;
+                    } else {
+                        System.out.println("Unknown argument '" + arg + "'.");
+                        return;    
+                    }
+            }
+            previousArg = arg;
+        }
     }
 }
